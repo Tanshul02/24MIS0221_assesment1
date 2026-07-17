22aad1e0655c79ab7421dbc0fb9f3167b44ac6dc
Tanshul Sharma <tanshulsharma02@gmail.com>
2026-07-17 13:17:50 +0530

    Added login feature

--- appointment.py
+++ appointment.py
@@ -3,3 +3,8 @@ print("Hospital Appointment Booking System")
 patient_name = input("Enter Patient Name: ")
 print("Welcome", patient_name)
 
+username = input("Enter Username: ")
+password = input("Enter Password: ")
+
+print("Login Successful")
+
