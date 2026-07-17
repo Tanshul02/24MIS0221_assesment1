b2ef0ed9f4af618be4948a09ad91094a58a4e358
Tanshul Sharma <tanshulsharma02@gmail.com>
2026-07-17 13:41:29 +0530

    Doctor Login

--- appointment.py
+++ appointment.py
@@ -1,10 +1,2 @@
-print("Hospital Appointment Booking System")
-
-patient_name = input("Enter Patient Name: ")
-print("Welcome", patient_name)
-
-username = input("Enter Username: ")
-password = input("Enter Password: ")
-
-print("Login Successful")
+print("Doctor Login")
 
